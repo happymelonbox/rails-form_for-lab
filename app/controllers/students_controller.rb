91c887fc1ff6afd9f939405c8bb3eb0215104ede
Justin Berger Howes <howes.j.j@gmail.com>
2021-10-09 10:27:00 +1100

    not working

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,31 @@
+class StudentsController < ApplicationController
+
+    def index
+        @students = Student.all
+    end
+
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.new(student_class_params)
+        @student.save
+        redirect_to student_path(@student)
+    end
+
+    def show
+        @student = Student.find(params[:id])
+    end
+
+    def update
+        @student = Student.find(params[:id])
+        @student.update(student_class_params)
+        redirect_to student_path(@student)
+    end
+
+    private
+    def student_class_params
+        params.require(:student).permit(:first_name, :last_name)
+    end
+end
